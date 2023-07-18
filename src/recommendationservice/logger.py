59a2c31084828a0639542a80acb36c309d618efb
Pierre Quemard <pierre.quemard@datadoghq.com>
2023-07-18 19:15:59 +0200

    update logger python

--- src/recommendationservice/logger.py
+++ src/recommendationservice/logger.py
@@ -33,7 +33,8 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
 def getJSONLogger(name):
   logger = logging.getLogger(name)
   handler = logging.StreamHandler(sys.stdout)
-  formatter = CustomJsonFormatter('%(timestamp)s %(severity)s %(name)s %(message)s')
+  # formatter = CustomJsonFormatter('%(timestamp)s %(severity)s %(name)s %(message)s')
+  formatter = CustomJsonFormatter('%(severity)s %(name)s %(message)s')
   handler.setFormatter(formatter)
   logger.addHandler(handler)
   logger.setLevel(logging.INFO)
