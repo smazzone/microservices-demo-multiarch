66d4069774ba6a3087e378f9f9951f71381f614c
Pierre Quemard <pierre.quemard@datadoghq.com>
2023-07-18 14:12:35 +0200

    Update to datadog config

--- dash/microservices/config_microservices.sh
+++ dash/microservices/config_microservices.sh
@@ -44,6 +44,7 @@ sudo hostnamectl set-hostname $TEAM_NAME-dash2023
 
 # Install agent
 helm repo add datadog https://helm.datadoghq.com
+helm repo add stable https://charts.helm.sh/stable 
 helm repo update
 
 # Start agent with datadog-values.yaml
