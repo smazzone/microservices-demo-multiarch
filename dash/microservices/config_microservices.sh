18a1c795fac346753c3ec66573ec8fc89739a131
Pierre Quemard <pierre.quemard@datadoghq.com>
2023-07-18 14:32:57 +0200

    remove deprecated value in dd conf

--- dash/microservices/config_microservices.sh
+++ dash/microservices/config_microservices.sh
@@ -58,5 +58,10 @@ skaffold run --platform=linux/amd64
 IP_ADDR=$(ip addr show enX0 | grep "inet " | awk -F'[:{ /}]+' '{ print $3 }')
 kubectl port-forward --address $IP_ADDR service/frontend 8080:80 &
 
+# Add POD Agent pod address
+AGENT_POD=$(kubectl get pods | sed -e '/datadog-agent/!d' | sed -n '/cluster/!p' | sed -n '/metrics/!p' | awk -F' ' '{print $1}')
+echo "alias datadog-status='kubectl exec \$AGENT_POD -- agent status'" >> ~.bashrc
+
 # add Flag to ENV
-echo "export DD_CTF='LEGENDOFBITS_TEARSOFSRE'" >> .bashrc
+echo "export DD_CTF='LEGENDOFBITS_TEARSOFSRE'" >> ~.bashrc
+source .bashrc
