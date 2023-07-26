10f5929891876d8dcf7b5a015e7b3142ba3a2590
chrisluekermann <chris.luekermann@datadoghq.com>
2023-07-26 17:24:17 +0200

    first version of new shell spawn

--- src/paymentservicejava/src/main/java/com/hipstershop/paymentservicejava/PaymentservicejavaApplication.java
+++ src/paymentservicejava/src/main/java/com/hipstershop/paymentservicejava/PaymentservicejavaApplication.java
@@ -2,8 +2,10 @@ package com.hipstershop.paymentservicejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PaymentservicejavaApplication {
 
 	public static void main(String[] args) {
