5dfc403cb68845ebda850806f69f539c2ef56195
Pierre Quemard <pierre.quemard@datadoghq.com>
2023-07-27 16:52:50 +0200

    add to update the reset of nginx

--- update.sh
+++ update.sh
@@ -7,3 +7,9 @@ helm upgrade datadog-agent -f dash/datadog-values.yaml datadog/datadog
 ## Restart kubernetes Deployment and Services
 echo "> reloading kubernetes cluster"
 skaffold run --platform=linux/amd64
+echo "> Configuring extras"
+# Setting variable to check agent status
+export AGENT_POD=$(kubectl get pods | sed -e '/datadog-agent/!d' | sed -n '/cluster/!p' | sed -n '/metrics/!p' | awk -F' ' '{print $1}')
+# Configure nginx
+export FRONTEND_LB=$(minikube service frontend-lb --url)
+sudo -E ./dash/microservices/conf_nginx.sh
